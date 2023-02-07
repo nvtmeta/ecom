0bb47ea4ffbbc4b1f5c1e502812b9f0afd8d0c00
nguyen viet thinh <112790614+nvtmeta@users.noreply.github.com>
2023-02-07 16:02:51 +0700

    1com

---
+++ pages/Success.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { BsBagCheckFill } from "react-icons/bs";
+import { useStateContext } from "@/context/StateContext";
+import Link from "next/link";
+import { runFireworks } from "@/lib/utils";
+const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+  const [order, setOrder] = useState(null);
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+    runFireworks();
+  }, []);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email for the receipt</p>
+        <p className="description">
+          If you have any question, please email
+          <a className="email" href="mailto:thinhdkbox@gmail.com">
+            thinhdkbox@gmail.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
---
+++ sanity-project/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ytx58qd8',
+    dataset: 'production'
+  }
+})
